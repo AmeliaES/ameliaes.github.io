5320bf3c4558d7e79a027c51b53f2caeec253386
Amelia Edmondson-Stait <edmondson.staita@gmail.com>
2026-05-07 17:32:41 +0100

    update website

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,8 +1,8 @@
 /*!
-* Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
-* Copyright 2013-2025 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
+ * Copyright 2013-2026 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+ */
 //
 // Scripts
 //
@@ -53,11 +53,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 // -------------------------------
 // Typewriter effect
 
-const words = [
-    "Post-Doctoral Researcher",
-    "Aspiring Software Developer",
-    "Aspiring Software Tester",
-];
+const words = ["Full-Stack Software Engineer"];
 
 const typewriterElement = document.getElementById("typewriter");
 
